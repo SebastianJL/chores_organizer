e7a544fa5dc18809fb706c899a1f89da9ad58fce
SebastianJL <johannes.lade@yahoo.com>
2022-10-12 15:34:37 +0200

    Add separate tables for Johannes and Linus.

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,13 @@
 use std::vec;
 
-use eframe::egui;
+use eframe::{
+    egui::{self, CentralPanel, Context, RichText, Ui},
+    epaint::FontId,
+};
 use egui_extras::{Size, TableBuilder};
 use log::info;
 
-use crate::chore::Chore;
+use crate::chore::{Chore, Owner};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -20,10 +23,17 @@ impl Default for App {
                 Chore {
                     name: "Staubsaugen".to_owned(),
                     due: "Today".to_owned(),
+                    owner: Owner::Linus,
                 },
                 Chore {
                     name: "Bad".to_owned(),
                     due: "Tomorrow".to_owned(),
+                    owner: Owner::Linus,
+                },
+                Chore {
+                    name: "Boden".to_owned(),
+                    due: "Tomorrow".to_owned(),
+                    owner: Owner::Johannes,
                 },
             ],
         }
@@ -54,49 +64,74 @@ impl eframe::App for App {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::CentralPanel::default().show(ctx, |ui| {
+        CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
             egui::warn_if_debug_build(ui);
 
-            TableBuilder::new(ui)
-                .striped(true)
-                .column(Size::at_least(Size::relative(0.3), 100.0))
-                .column(Size::at_least(Size::relative(0.3), 100.0))
-                .column(Size::remainder())
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.heading("Chore");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Due");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Actions");
-                    });
-                })
-                .body(|mut body| {
-                    for chore in self.chores.iter() {
-                        body.row(30.0, |mut row| {
-                            row.col(|ui| {
-                                ui.label(format!("{}", chore.name));
-                            });
-                            row.col(|ui| {
-                                ui.label(format!("{}", chore.due));
-                            });
-                            row.col(|ui| {
-                                if ui.button("done").clicked() {
-                                    info!("Done button clicked");
-                                }
-                            });
-                        });
-                    }
-                });
+            ui.label(RichText::new("Linus").font(FontId::proportional(30.0)));
+            ui.group(|ui| {
+                let chores_linus: Vec<Chore> = self
+                    .chores
+                    .iter()
+                    .cloned()
+                    .filter(|chore| chore.owner == Owner::Linus)
+                    .collect();
+                ui.push_id(1, |ui| build_table(ui, &chores_linus));
+            });
+            ui.separator();
+            ui.label(RichText::new("Johannes").font(FontId::proportional(30.0)));
+            ui.group(|ui| {
+                let chores_johannes: Vec<Chore> = self
+                    .chores
+                    .iter()
+                    .cloned()
+                    .filter(|chore| chore.owner == Owner::Johannes)
+                    .collect();
+                ui.next_auto_id();
+                ui.push_id(2, |ui| build_table(ui, &chores_johannes));
+            });
         });
     }
+}
+
+fn build_table(ui: &mut Ui, chores: &[Chore]) {
+    TableBuilder::new(ui)
+        .striped(true)
+        .column(Size::at_least(Size::relative(0.3), 100.0))
+        .column(Size::at_least(Size::relative(0.3), 100.0))
+        .column(Size::remainder())
+        .header(20.0, |mut header| {
+            header.col(|ui| {
+                ui.heading("Chore");
+            });
+            header.col(|ui| {
+                ui.heading("Due");
+            });
+            header.col(|ui| {
+                ui.heading("Actions");
+            });
+        })
+        .body(|mut body| {
+            for chore in chores.iter() {
+                body.row(30.0, |mut row| {
+                    row.col(|ui| {
+                        ui.label(chore.name.to_string());
+                    });
+                    row.col(|ui| {
+                        ui.label(chore.due.to_string());
+                    });
+                    row.col(|ui| {
+                        if ui.button("done").clicked() {
+                            info!("Done button clicked");
+                        }
+                    });
+                });
+            }
+        });
 }
--- src/chore.rs
+++ src/chore.rs
@@ -1,5 +1,14 @@
-#[derive(serde::Deserialize, serde::Serialize)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Chore {
     pub name: String,
     pub due: String,
+    pub owner: Owner,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone)]
+pub enum Owner {
+    Linus,
+    Johannes
 }
